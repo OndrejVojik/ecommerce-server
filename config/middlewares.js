61bf40991b95cb56f826112398e8939f8e805b2b
OndrejVojik <vojik.cz@gmail.com>
2024-11-17 17:00:13 +0100

    Update CORS settings to allow requests from localhost:5173

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,8 +1,29 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:', 'http:', 'http://localhost:5173'],
+          'img-src': ["'self'", 'data:', 'blob:', 'http://localhost:5173'],
+          'media-src': ["'self'", 'data:', 'blob:', 'http://localhost:5173'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['http://localhost:5173'], // Allow requests from your local development environment
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization'],
+      keepHeadersOnError: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
